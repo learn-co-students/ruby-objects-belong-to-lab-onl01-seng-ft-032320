c267b82613b800d0e2fbe2e98379269b2a3f46b7
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-06 17:53:31 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialization(title)
+    @title = title
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialization(title)
+    @title = title
+  end
+
+end
